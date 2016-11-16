3725e8aceb5366d14fb76bccd2ce12facaf6ff69
kwuz <young.mikaila@gmail.com>
2016-11-16 15:30:06 -0700

    cell/row/collumn/block structure basic

---
+++ OO_MYoung_HW4/Cell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    public class Cell
+    {
+        public char value;
+        public List<char> possibleValues;
+        public int row;
+        public int collumn;
+        public int block;
+
+        public void clearAllPossibilities()
+        {
+            possibleValues.Clear();
+        }
+
+        public void clearPossibility(char num)
+        {
+            if (possibleValues.Contains(num))
+                possibleValues.Remove(num);
+        }
+    }
+
+}
