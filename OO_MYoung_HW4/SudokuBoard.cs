ce649d593ba477a74b769d1e4ab9b3748b9b870e
kwuz <young.mikaila@gmail.com>
2016-11-16 16:09:40 -0700

    cell iEnumerator

--- OO_MYoung_HW4/SudokuBoard.cs
+++ OO_MYoung_HW4/SudokuBoard.cs
@@ -1,8 +1,27 @@
-﻿namespace SudokuSolver
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace SudokuSolver
 {
     public class SudokuBoard
     {
-      public int size { get; set; }
+        public int size { get; set; }
+        public List<char> header;
+        public List<Block> collumns;
+        public List<Block> rows;
+        public List<Block> blocks;
+
+        public void toString()
+        {
+            foreach(Block row in rows){
+                foreach(Cell cell in row)
+                {
+                    Console.Write(cell.value);
+                }
+                Console.Write('\n');
+            }
+        }
     }
 
    // SudokuBoard loadBoard(inputFile)
